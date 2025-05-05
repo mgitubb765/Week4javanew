f3a8305040f57fdb7432b7c263edd1d9cd9a25eb
mgitubb765 <maka.4king@gmail.com>
2025-05-05 10:47:52 -0400

    Update ClassesController.java

--- Week4Javaupdate/cpt_245_week_4-main/src/main/java/edu/cmcc/cpt/cpt_245_week_4/Classes/ClassesController.java
+++ Week4Javaupdate/cpt_245_week_4-main/src/main/java/edu/cmcc/cpt/cpt_245_week_4/Classes/ClassesController.java
@@ -18,9 +18,42 @@ public class ClassesController {
         String sql = "SELECT * FROM classes;";
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             Classes classes = new Classes();
-            classes.setId(rs.getInt("id"));
-            classes.setName(rs.getString("name"));
-            classes.setInstructor(rs.getString("instructor"));
+            classes.setClassCode(rs.getString("class_code"));
+            classes.setClassName(rs.getString("class_name"));
+            return classes;
+        });
+    }
+
+    @GetMapping("/{class_code}")
+    public Classes getClassByCode(@PathVariable String class_code) {
+        String sql = "SELECT * FROM classes WHERE class_code = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{class_code}, (rs, rowNum) -> {
+            Classes classes = new Classes();
+            classes.setClassCode(rs.getString("class_code"));
+            classes.setClassName(rs.getString("class_name"));
+            return classes;
+        });
+    }
+
+    @PostMapping
+    public void createClass(@RequestBody Classes classes) {
+        String sql = "INSERT INTO classes (class_code, class_name) VALUES (?, ?)";
+        jdbcTemplate.update(sql, classes.getClassCode(), classes.getClassName());
+    }
+
+    @PutMapping("/{class_code}")
+    public void updateClass(@PathVariable String class_code, @RequestBody Classes classes) {
+        String sql = "UPDATE classes SET class_name = ? WHERE class_code = ?";
+        jdbcTemplate.update(sql, classes.getClassName(), class_code);
+    }
+
+    @DeleteMapping("/{class_code}")
+    public void deleteClass(@PathVariable String class_code) {
+        String sql = "DELETE FROM classes WHERE class_code = ?";
+        jdbcTemplate.update(sql, class_code);
+    }
+}
+
             classes.setCredits(rs.getInt("credits"));
             return classes;
         });
